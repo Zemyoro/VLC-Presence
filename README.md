d07067152e567848a04f1f6a19a47c3f7f05757a
Zemyoro <85952603+Zemyoro@users.noreply.github.com>
2022-05-29 12:55:08 +0900

    .

--- README.md
+++
@@ -1,3 +0,0 @@
-### Setup
-
-Edit the config/config.json to include the appropriate tokens and VLC path.
--- src/helpers/error.ts
+++
@@ -1,6 +0,0 @@
-import * as config from '../../config/config.json';
-
-export default function (type: string, content: string) {
-    if (!config.verbose) return;
-    console.log(`[${type}] ${content}`);
-}
